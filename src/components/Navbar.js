948d3f1aa37870db310acf945af6c8643d9550a8
Andrew Pomeroy <andrewmichaelpomeroy@gmail.com>
2020-09-08 13:10:29 -0700

    work navbar item styles to be a bit more DRY

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -40,6 +40,8 @@ const LogoContainer = styled.div`
   align-items: center;
 `
 
+const NAVBAR_ITEM_GUTTER = 32;
+
 const NavbarCenterColumnWrap = styled(CenterColumnWrap)`
   flex: 1;
   max-height: 100%;
@@ -51,17 +53,20 @@ const NavbarCenterColumn = styled(CenterColumn)`
 const NavbarInline = styled(Inline)`
   max-height: 100%;
 `;
+NavbarInline.defaultProps.gutter = NAVBAR_ITEM_GUTTER;
 const NavbarInlineSplit = styled(Inline)`
   flex: 1;
   max-height: 100%;
   max-width: 100%;
   width: 100%;
   justify-content: space-between;
-`;
+  `;
+NavbarInlineSplit.defaultProps.gutter = NAVBAR_ITEM_GUTTER;
 const NavbarInlineItem = styled(InlineItem)`
   display: flex;
   max-height: 100%;
 `;
+NavbarInlineItem.defaultProps.gutter = NAVBAR_ITEM_GUTTER;
 const NavbarInlineLink = styled(Link)`
   display: flex;
   align-items: center;
@@ -70,33 +75,45 @@ const NavbarInlineLink = styled(Link)`
   font-weight: 600;
   letter-spacing: .03em;
   text-transform: uppercase;
-  /* color: ${props => props.theme.colors.text}; */
-  /* color: ${props => makeLinkColors(props.theme.colors.text)}; */
+  color: ${props => props.theme.colors.text};
+  &:hover, &:focus {
+    color: ${props => Color(props.theme.colors.gold).string()};
+  }
+  &:active {
+    color: ${props => Color(props.theme.colors.gold).darken(.25).string()};
+  }
+  /* &:hover, &:focus {
+    color: ${props => Color(props.theme.colors.text).lighten(.65).string()};
+  }
+  &:active {
+    color: ${props => Color(props.theme.colors.text).darken(.65).string()};
+  } */
 `;
-NavbarInlineLink.defaultProps = {
-  color: "text"
-}
+// NavbarInlineLink.defaultProps = {
+//   color: "text"
+// }
 
 const Navbar = props => {
   return (
   <NavbarOuter>
     <NavbarCenterColumnWrap>
       <NavbarCenterColumn>
-        <NavbarInlineSplit gutter={24}>
-          <NavbarInlineItem gutter={24} to="/" as={Link}>
+        <NavbarInlineSplit>
+          <NavbarInlineItem to="/" as={Link}>
             <LogoContainer>
               <Logo src="/img/header-logo.png" alt="Mercury Free Mining" />
             </LogoContainer>
           </NavbarInlineItem>
           <NavbarInlineItem style={{ display: 'flex' }}>
-            <NavbarInline gutter={24}>
-              <NavbarInlineItem gutter={24}>
+            {/* <NavbarInline gutter={24}> */}
+            <NavbarInline>
+              <NavbarInlineItem>
                 <NavbarInlineLink to="/products">Products</NavbarInlineLink>
               </NavbarInlineItem>
-              <NavbarInlineItem gutter={24}>
+              <NavbarInlineItem>
                 <NavbarInlineLink to="/blog">Blog</NavbarInlineLink>
               </NavbarInlineItem>
-              <NavbarInlineItem gutter={24}>
+              <NavbarInlineItem>
                 <NavbarInlineLink to="/contact">Contact</NavbarInlineLink>
               </NavbarInlineItem>
             </NavbarInline>
